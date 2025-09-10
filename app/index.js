cc25d16cd524940fc54d4e344d6fe523d1812c8b
Ittipat Jidrada <ittipat@sirisoft.co.th>
2025-09-10 16:22:56 +0700

    start: ngins

--- app/index.js
+++ app/index.js
@@ -7,12 +7,22 @@ const port = 3000;
 // Parse JSON body
 app.use(bodyParser.json());
 
+// Middleware to transform request body (username -> name)
+app.use((req, res, next) => {
+  if (req.body && req.body.username !== undefined) {
+    console.log('Transforming request: username -> name');
+    req.body.name = req.body.username;
+    delete req.body.username;
+  }
+  next();
+});
+
 // Sample POST endpoint
 app.post('/api', (req, res) => {
-  console.log('Original request body:', req.body);
+  console.log('Request body after transformation:', req.body);
   res.json({
     message: 'Hello, ' + req.body.name,
-    receivedField: 'name',
+    receivedField: 'name', // This will be transformed to 'username' by Nginx
     timestamp: new Date().toISOString()
   });
 });
